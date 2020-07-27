8c5c604e64a4c31c70c21b637229efa2203ba3fe
Oliv0533 <oliverchristensen93@gmail.com>
2020-07-27 16:55:38 +0200

    Scaffolded Identity to be able to change login and other pages

---
+++ RegistrationApp/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using RegistrationAppDAL.Data;
+using RegistrationAppDAL.Models;
+
+[assembly: HostingStartup(typeof(RegistrationApp.Areas.Identity.IdentityHostingStartup))]
+namespace RegistrationApp.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
